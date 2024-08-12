26b0bb0fa6e21df24e3c4be2c76ccabe40c7507c
Hamdan Fadhillah <hamdanfadh@gmail.com>
2024-08-12 18:30:31 +0700

    add compose

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,11 +1,13 @@
 import uuid
 import requests
+import json
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+import time
+import logging
 
-
-transaction_dags = DAG(
+dag = DAG(
     dag_id='dag_1',
     start_date = datetime.datetime(2024, 8, 5),
     schedule = '@daily',
@@ -14,6 +16,7 @@ transaction_dags = DAG(
     }
 )
 
+#Task
 def get_data():
     result = requests.get('https://randomuser.me/api')
     result = result.json()
@@ -25,13 +28,13 @@ def data_formatting(result):
     location = result['location']
     data['first_name'] = result['name']['first']
     data['last_name'] = result['name']['last']
-    data['dob'] = res['dob']['date']
+    data['dob'] = result['dob']['date']
     data['gender'] = result['gender']
-    data['email'] = res['email']
-    data['username'] = res['login']['username']
-    data['registered_date'] = res['registered']['date']
-    data['phone'] = res['phone']
-    data['picture'] = res['picture']['large']
+    data['email'] = result['email']
+    data['username'] = result['login']['username']
+    data['registered_date'] = result['registered']['date']
+    data['phone'] = result['phone']
+    data['picture'] = result['picture']['large']
     data['city'] = location['city']
     data['state'] = location['state']
     data['postcode'] = location['postcode']
@@ -39,4 +42,32 @@ def data_formatting(result):
     data['street'] = f"{str(location['street']['number'])} {location['street']['name']}"
     return data
 
+def stream_data():
+    import json
+    from kafka import KafkaProducer
+    import time
+    import logging
 
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    curr_time = time.time()
+
+    while True:
+        if time.time() > curr_time + 60: #1 minute
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
+
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occured: {e}')
+            continue
+
+task_1 = PythonOperator(
+ task_id='task_1',
+ python_callable=stream_data,
+ dag=dag,
+)
+
+# task dependencies
+task_1
